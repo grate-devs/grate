e6d145e377fe413da2cb79d485b128bf7b646cf1
Herman Crawfurd Svensen <h@herms.no>
2026-04-18 21:52:48 +0200

    Fix #728: Add schema name clause when deciding whether Status column exists on Version table. (#729)

--- src/grate.sqlserver/Bootstrapping/Sql/GrateStructure/up/01_version_add_status_column.sql
+++ src/grate.sqlserver/Bootstrapping/Sql/GrateStructure/up/01_version_add_status_column.sql
@@ -1,6 +1,7 @@
 IF NOT EXISTS(
    SELECT 1 FROM sys.columns WHERE name = 'status'
-   AND OBJECT_NAME(object_id) = '{{VersionTable}}')
+   AND OBJECT_NAME(object_id) = '{{VersionTable}}'
+   AND OBJECT_SCHEMA_NAME(object_id) = '{{SchemaName}}')
 BEGIN
     ALTER TABLE {{SchemaName}}.{{VersionTable}}
     ADD status nvarchar(50) NULL
